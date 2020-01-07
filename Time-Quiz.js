fbf93661df123b48310cdd888e3782efe87125a8
yan zhao <yzhao322@gmail.com>
2020-01-06 20:34:33 -0800

    add view highscore

--- Time-Quiz.js
+++ Time-Quiz.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     
     var questions = [
         {
@@ -7,9 +7,9 @@ $( document ).ready(function() {
             answer: "alerts",
         },
         {
-          title: "The condition in an if / else statement is enclosed within ____.",
-          choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-          answer: "parentheses",
+            title: "The condition in an if / else statement is enclosed within ____.",
+            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+            answer: "parentheses",
         },
         {
             title: "JavaScript is loosely typed Language and have more relaxed syntax and rules.",
@@ -17,71 +17,64 @@ $( document ).ready(function() {
             answer: "True",
         },
         {
-            title: "JavaScript is loosely typed Language and have more relaxed syntax and rules.",
-            choices: ["True", "False"],
-            answer: "True",
+            title: "The undefined and null are equal in value but different in type. The type of null is ____.",
+            choices: ["strings", "undefined", "objects"],
+            answer: "objects",
         },
         {
-            title: "JavaScript is loosely typed Language and have more relaxed syntax and rules.",
-            choices: ["True", "False"],
-            answer: "True",
+            title: "The undefined and null are equal in value but different in type. The type of underfine is ____.",
+            choices: ["strings", "undefined", "objects"],
+            answer: "undefined",
         },
-      ];
+    ];
       
-    var button = $('button');
+    var button = $('#Start');
     var h1 = $('h1');
-    var h2 = $('h2');
+    var highScore = $('.Highscore');
+    var h2 = $('.TimeRemaining');
     var h3 = $('h3');
     var h4 = $('h4');
     var h5 = $('h5');
-    var count;
+    var count = 0;
     var i;
     var times;
     var score;
     var localSN = 0;
 
+    highScore.append('View Highscores');
+    h2.append('Time Remain ' + count);
     $('hr').hide();
-        h1.click(function () {
-            h1.hide();
-            
-            i = 0; 
-            times = 0;
-            count = 75;
-            score = 0;
-            var timer = setInterval(function () {
-                h2.empty().append('Time Remain ' + count);
-                h2.value = count--;
-                if (times === 5) {
-                    alert("You completed quiz!");
-                    score = count;
-                    clearInterval(timer);
-                    allDone();
-                }
-                else if (count <= 0) {
-                    alert("Time Out!");
-                    clearInterval(timer);
-                    allDone();
-                }
-            }, 1000);
 
-            // $.each(questions, function (key, value) {
-            //     console.log(key, value);
-            //     console.log(questions[5].title);
-            //     console.log(questions[5].answer);
-            
-            // });
+    button.click(function () {
+        h1.hide();
+        i = 0;
+        times = 0;
+        count = 100;
+        score = 0;
+        var timer = setInterval(function () {
+            h2.empty().append('Time Remain ' + count);
+            h2.value = count--;
+            if (times === 5) {
+                score = count;
+                clearInterval(timer);
+                allDone();
+            }
+            else if (count <= 0) {
+                alert("Time Out!");
+                clearInterval(timer);
+                allDone();
+            }
+        }, 1000);
+        quizQuestion();
        
-            quizQuestion();
-       
-       
-        });
+    });
 
     function quizQuestion() {
         
         if (i < 5) {
             var div = $('<div>');
             div.text(i + 1 + ". " + questions[i].title);
-            div.css('padding', '10px');
+            div.css('padding', '30px');
             h3.empty().append(div);
         
         
@@ -91,7 +84,10 @@ $( document ).ready(function() {
                 div.append(btn);
                 btn.css('display', 'block');
                 btn.css('margin', '30px');
-                btn.css('margin-left', '100px');
+                btn.css('margin-left', '80px');
+                if ($(window).width() <= 600) {
+                    btn.css('width', '60%');
+                }
             }
             
             div.click(function (event) {
@@ -109,8 +105,8 @@ $( document ).ready(function() {
                     count = count - 15;
                     times = times + 1;
                     i++;
-                    $('hr').show();
-                    h4.text('Wrong!');  
+                    h4.text('Wrong!').show(1).delay(300).hide(1);
+                    $('hr').show(1).delay(300).hide(1);
                     quizQuestion();
                     
                 }
@@ -118,7 +114,7 @@ $( document ).ready(function() {
             });
         }
         else {
-                allDone();
+            allDone();
             
         }
     
@@ -127,6 +123,7 @@ $( document ).ready(function() {
     function allDone() {
         $('hr').hide();
         h2.hide();
+        highScore.hide();
         h4.hide();
         var div = $('div');
         div.text("All Done!");
@@ -140,13 +137,14 @@ $( document ).ready(function() {
         h5.css('text-align', 'center');
         $('#submitScore').click(function () {
             var name = $('#name').val();
-            var objNAS = { 'name': name, 'score': score };
-            localStorage.setItem("objNAS" + localSN, JSON.stringify(objNAS));
-            highScore();
+            var nameAndScore = { 'name': name, 'score': score };
+            localStorage.setItem('nameAndScore' + localSN, JSON.stringify(nameAndScore));
+            highScores();
 
         });
     }
-    function highScore() {
+    
+    function highScores() {
         h3.hide();
         h5.hide();
         var h6 = $('<h6>');
@@ -154,31 +152,124 @@ $( document ).ready(function() {
         h6.css('font-size', '50px');
         h6.css('margin', '30px');
         $('body').append(h6);
-        var storeNameAndScore = JSON.parse(localStorage.getItem("objNAS"+ localSN));
         var ol = $('<ol>');
-        var li = $('<li>');
         h6.append(ol);
-        ol.append(li.text(storeNameAndScore.name + " - " + "(" + storeNameAndScore.score + ")"));
+        var int = localSN;
+        for (var i = 100; i >= 0; i--) {
+            if (localStorage.getItem("nameAndScore" + i) === null) {
+                
+            }
+            else {
+                var nameAndScore = JSON.parse(localStorage.getItem('nameAndScore' + i));
+                var li = $('<li>');
+                li.attr('class', nameAndScore.score);
+                li.text(nameAndScore.name + " - " + nameAndScore.score);
+                ol.append(li);
+            }
+        }
         ol.css('font-size', '20px');
+        ol.css('font-weight', '200');
+        var items = $('ol > li').get();
+        items.sort(function (a, b) {
+            var A = $(a);
+            var B = $(b);
+          
+            if (A[0].className < B[0].className) return 1;
+            if (A[0].className > B[0].className) return -1;
+            return 0;
+        });
+        
+        $.each(items, function (i, li) {
+            ol.append(li); /* This removes li from the old spot and moves it */
+        });
+
         var div = $('<div>');
         $('<button>').attr('id', 'goBack').text("Go Back").appendTo(div);
         $('<button>').attr('id', 'clearHighScore').text("Clear Highscores").appendTo(div);
         $('body').append(div);
-        $('#goBack').click(function (event) {
-            event.preventDefault();
-            h2.empty().show();
+        $('#goBack').click(function () {
+            count = 0;
+            h2.empty().text("Time Remain 0").show();
+            highScore.show();
             h3.empty().show();
             h4.empty().show();
             h5.empty().show();
             h6.remove();
             div.remove();
             h1.show();
-            localSN++;  
+            localSN++;
         });
         $('#clearHighScore').click(function (event) {
             event.preventDefault();
-            $('ol').hide();
+            for (var i = localSN; i >= 0; i--) {
+                localStorage.removeItem('nameAndScore' + i);
+            }
+            ol.remove();
         })
+
+   
         
     }
+
+    highScore.click(function () {
+        button.hide();
+        h1.hide();
+        highScore.hide();
+        h2.hide();
+        h3.hide();
+        h4.hide();
+        h5.hide();
+        var h7 = $('<h7>');
+        $('body').append(h7);
+        h7.text("Highscores");
+        h7.css('font-size', '50px');
+        h7.css('margin', '30px');
+        var ol = $('<ol>');
+        h7.append(ol);
+        for (var i = 100; i >= 0; i--) {
+            if (localStorage.getItem("nameAndScore" + i) === null) {
+                
+            }
+            else {
+                var nameAndScore = JSON.parse(localStorage.getItem('nameAndScore' + i));
+                var li = $('<li>');
+                li.attr('class', nameAndScore.score);
+                li.text(nameAndScore.name + " - " + nameAndScore.score);
+                ol.append(li);
+            }
+        }
+        ol.css('font-size', '20px');
+        ol.css('font-weight', '200');
+        var items = $('ol > li').get();
+        console.log(items);
+        items.sort(function (a, b) {
+            var A = $(a);
+            var B = $(b);
+          
+            if (A[0].className < B[0].className) return 1;
+            if (A[0].className > B[0].className) return -1;
+            return 0;
+        });
+        ol.css('margin', '30px');
+        var div = $('<div>');
+        $('<button>').attr('id', 'Back').text("Back").appendTo(div);
+        $('body').append(div);
+        $('#Back').click(function () {
+            button.show();
+            h1.show();
+            highScore.show();
+            h2.show();
+            h3.show();
+            h4.show();
+            h5.show();
+            h7.remove();
+            div.remove();
+            if (count !== 0) {
+                h1.hide();
+            }
+
+        });
+    });
+      
+    
 })
